87bf2a9a18936201f0303bd452f88c4d7b2125fb
MArslan <gery_maz_555@hotmail.com>
2020-12-07 22:19:35 +0500

    included the Dots on the sliding pages

--- app/src/main/java/com/mhdarslan/onboardingscreendesign/MainActivity.java
+++ app/src/main/java/com/mhdarslan/onboardingscreendesign/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager mSlideViewPager;
     private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
 
     private SliderAdapter sliderAdapter;
 
@@ -25,5 +29,44 @@ public class MainActivity extends AppCompatActivity {
 
         mSlideViewPager.setAdapter(sliderAdapter);
 
+        // call the 'addDotsIndicator' method
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
     }
+
+    public void addDotsIndicator(int position){
+
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+        for(int i=0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length >0){
+            mDots[position].setTextColor(getResources().getColor(R.color.white));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
