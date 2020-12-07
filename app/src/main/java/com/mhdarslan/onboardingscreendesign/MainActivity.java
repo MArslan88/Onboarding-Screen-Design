b2ff289c9a5b728a31ed3a0b2734777d926d2008
MArslan <gery_maz_555@hotmail.com>
2020-12-07 23:44:03 +0500

    included the Btn functionality

--- app/src/main/java/com/mhdarslan/onboardingscreendesign/MainActivity.java
+++ app/src/main/java/com/mhdarslan/onboardingscreendesign/MainActivity.java
@@ -3,8 +3,11 @@ package com.mhdarslan.onboardingscreendesign;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     private SliderAdapter sliderAdapter;
 
+    private Button mNextBtn, mBackBtn, mStartBtn;
+    private int mCurrentPage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         mSlideViewPager = findViewById(R.id.slideViewPager);
         mDotLayout = findViewById(R.id.dotsLayout);
+
+        mNextBtn = findViewById(R.id.nextBtn);
+        mBackBtn = findViewById(R.id.prevBtn);
+        mStartBtn = findViewById(R.id.startBtn);
 
         sliderAdapter = new SliderAdapter(this);
 
@@ -33,6 +43,21 @@ public class MainActivity extends AppCompatActivity {
         addDotsIndicator(0);
 
         mSlideViewPager.addOnPageChangeListener(viewListener);
+
+        // onClickListeners
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideViewPager.setCurrentItem(mCurrentPage +1);
+            }
+        });
+
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideViewPager.setCurrentItem(mCurrentPage -1);
+            }
+        });
     }
 
     public void addDotsIndicator(int position){
@@ -62,6 +87,48 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPageSelected(int position) {
             addDotsIndicator(position);
+            mCurrentPage = position;
+
+            if(position == 0){
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(false);
+                mBackBtn.setVisibility(View.INVISIBLE);
+                mNextBtn.setVisibility(View.VISIBLE);
+                mStartBtn.setVisibility(View.INVISIBLE);
+
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("");
+
+            }else if(position == mDots.length -1){
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+                mStartBtn.setVisibility(View.VISIBLE);
+                mNextBtn.setVisibility(View.INVISIBLE);
+
+                mNextBtn.setText("Finish");
+                mBackBtn.setText("Back");
+
+                mStartBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                        finish();
+                    }
+                });
+
+            }else {
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+                mNextBtn.setVisibility(View.VISIBLE);
+                mStartBtn.setVisibility(View.INVISIBLE);
+
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("Back");
+            }
         }
 
         @Override
